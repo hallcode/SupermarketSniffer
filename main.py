6c412e5941edfaef97fdecfa25020a124f3ba15a
hallcode <alex@alexhall.dev>
2023-10-28 14:06:00 +0100

    Minor changes

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from brands import brands
 from product import search_categories
 from scanner import scan
-
+from playwright.sync_api import sync_playwright
 
 def start_scan():
-    for brand in brands:
-        for category in search_categories:
-            scan(brand, category, headless=True)
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False)
+
+        for brand in brands:
+            for category in search_categories:
+                page = browser.new_page()
+                scan(page, brand, category)
+                page.close()
+
+        browser.contexts.clear()
+        browser.close()
 
 
 if __name__ == "__main__":
--- scanner.py
+++ scanner.py
@@ -4,44 +4,37 @@ from datetime import date
 
 from brands import Brand
 from product import Category, Product
-from playwright.sync_api import sync_playwright
 
 
-def scan(brand: Brand, category: Category, headless=True):
+def scan(page, brand: Brand, category: Category):
     print("Starting " + brand.name)
 
     output_path = os.path.join(os.getcwd(), "output", date.today().isoformat())
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    with sync_playwright() as p:
-        browser = p.firefox.launch(headless=headless)
-        page = browser.new_page()
-        page.goto(brand.start_url)
+    page.goto(brand.start_url)
 
-        # Dismiss cookie notice
-        try:
-            cookie_button = page.get_by_role("button").get_by_text(
-                re.compile("allow|accept", re.IGNORECASE)
-            )
-            cookie_button.wait_for()
-            cookie_button = cookie_button.first
-            cookie_button.hover()
-            cookie_button.click()
-            page.wait_for_load_state("domcontentloaded")
-        except:
-            # If there's no cookie notice... we don't care
-            pass
+    # Dismiss cookie notice
+    try:
+        cookie_button = page.get_by_role("button").get_by_text(
+            re.compile("allow|accept", re.IGNORECASE)
+        )
+        cookie_button.wait_for()
+        cookie_button = cookie_button.first
+        cookie_button.hover()
+        cookie_button.click()
+        page.wait_for_load_state("domcontentloaded")
+    except:
+        # If there's no cookie notice... we don't care
+        pass
 
-        # Try to find the search bar
-        search_input = page.get_by_placeholder(re.compile("search|find", re.IGNORECASE))
-        search_input = search_input.first
-        search_input.fill(category.search_term)
-        search_input.press("Enter")
-        page.wait_for_load_state("networkidle")
+    # Try to find the search bar
+    search_input = page.get_by_placeholder(re.compile("search|find", re.IGNORECASE))
+    search_input = search_input.first
+    search_input.fill(category.search_term)
+    search_input.press("Enter")
+    page.wait_for_load_state("networkidle")
 
-        screenshot_name = brand.name.lower() + "_" + category.name.lower() + ".png"
-        page.screenshot(path=os.path.join(output_path, screenshot_name))
-
-        browser.contexts.clear()
-        browser.close()
+    screenshot_name = brand.name.lower() + "_" + category.name.lower() + ".png"
+    page.screenshot(path=os.path.join(output_path, screenshot_name))
